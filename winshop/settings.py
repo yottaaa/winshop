39dc9c6df66f02bb17713d7b9eb65ac00342bcc2
Erick Bautista <yotterick7@gmail.com>
2022-01-06 18:14:01 +0800

    update settings

--- winshop/settings.py
+++ winshop/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*x&6iep*wb@q)su3atpunn&txt-$3mt7eg9=k)9hxbd0#zqv-1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'https://winshop.herokuapp.com'
+    'win-shop.herokuapp.com'
 ]
 
 
