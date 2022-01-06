9587ddef4185f1e2b12214ddc2ccbd4d497817c3
Erick Bautista <yotterick7@gmail.com>
2022-01-06 22:36:12 +0800

    update urls

--- winshop/urls.py
+++ winshop/urls.py
@@ -4,5 +4,5 @@ from core import views as core_views
 
 urlpatterns = [
     path('', core_views.index, name='landing-page'),
-    path('search_product/<str:keyword>', api_views.search_product, name="search-product"),
+    path('search_product/<str:keyword>/', api_views.search_product, name="search-product"),
 ]
